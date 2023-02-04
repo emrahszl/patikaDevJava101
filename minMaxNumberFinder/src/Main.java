bfb13b7d6addac6af0d16245ada7a8bdeac8348f
Emrah Sözlü <114095274+emrahszl@users.noreply.github.com>
2023-02-05 02:07:19 +0300

    Update Main.java

--- minMaxNumberFinder/src/Main.java
+++ minMaxNumberFinder/src/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int number, tempNumber, countNumber, maxNumber, minNumber = 0;
+        int number, countNumber, maxNumber = 0, minNumber = 0;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Kaç tane sayı gireceksiniz : ");
         countNumber = scanner.nextInt();
 
-        while (countNumber == 0 || countNumber == 1 || countNumber < 0) {
+        while (countNumber == 0 || countNumber < 0) {
 
             System.out.println("Hatalı giriş yaptınız, lütfen pozitif ve en az 2 sayı giriniz !");
 
@@ -20,26 +20,24 @@ public class Main {
 
         }
 
-        System.out.print("1. Sayıyı giriniz : ");
-        tempNumber = scanner.nextInt();
-        maxNumber = tempNumber;
-
-        for (int i = 2; i <= countNumber; i++) {
+        for (int i = 1; i <= countNumber; i++) {
 
             System.out.print(i + ". Sayıyı giriniz : ");
             number = scanner.nextInt();
 
-            if (number > maxNumber) {
-
-                maxNumber = number;
-
-                if (minNumber == 0 && i == 2) {
-
-                    minNumber = tempNumber;
-                }
-            } else if (number < minNumber) {
+            if (i == 1) {
 
                 minNumber = number;
+                maxNumber = number;
+
+            } else {
+
+                if (number > maxNumber) {
+
+                    maxNumber = number;
+                } else if (number < minNumber) {
+                    minNumber = number;
+                }
             }
         }
         System.out.println("En büyük sayı : " + maxNumber);
