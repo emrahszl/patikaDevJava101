3e6e8057a693da4a971459f8f8b43c25bb94cbf5
Emrah Sözlü <114095274+emrahszl@users.noreply.github.com>
2023-02-05 18:22:53 +0300

    perfect number finder

---
+++ perfectNumberFinder/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int number, total = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz : ");
+        number = scanner.nextInt();
+
+        for (int i = 1; i < number; i++) {
+
+            if (number % i == 0) {
+
+                total += i;
+
+            }
+        }
+
+        if (total == number) {
+
+            System.out.println(number + " mükemmel sayıdır.");
+
+        } else {
+
+            System.out.println(number + " mükemmel sayı değildir.");
+
+        }
+    }
+}
